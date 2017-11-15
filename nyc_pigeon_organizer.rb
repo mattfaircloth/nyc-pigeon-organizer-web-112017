7c1a8efb317d1c8a5f1a5fa13804d56aef2985eb
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-15 21:10:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  container = {}
+  data.each do |category, info_hash|
+  info_hash.each do |attribute, name_array|
+    name_array.each do |name|
+      pigeon_list[name] ||= {}
+      pigeon_list[name][category] ||= []
+      pigeon_list[name][category] << attribute.to_s
+    end
+  end
+ end
+ pigeon_list
 end
